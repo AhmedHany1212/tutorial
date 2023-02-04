c00908aeb97f60cb1fda4087ac44102c83457b52
AhmedHany1212 <117445830+AhmedHany1212@users.noreply.github.com>
2023-02-04 11:53:28 +0200

    Update ifc_coverage.py

--- tests/ifc_coverage.py
+++ tests/ifc_coverage.py
@@ -1,14 +1,17 @@
 import cocotb
-from cocotb.triggers import Timer, FallingEdge, RisingEdge, ReadOnly, NextTimeStep
+from cocotb.triggers import Timer, RisingEdge, ReadOnly, NextTimeStep, FallingEdge
 from cocotb_bus.drivers import BusDriver
-from cocotb_bus.monitors import BusMonitor
 from cocotb_coverage.coverage import CoverCross, CoverPoint, coverage_db
+from cocotb_bus.monitors import BusMonitor
 import os
+import random
 
-
+global case
+case=2
 def sb_fn(actual_value):
-    global expected_value
-    assert actual_value == expected_value.pop(0), "Scoreboard Matching Failed"
+    print("Recived value=",actual_value)
+    
+
 
 
 @CoverPoint("top.a",  # noqa F405
@@ -33,27 +36,34 @@ def cover(a, b):
 
 @cocotb.test()
 async def ifc_test(dut):
-    global expected_value
-    a = (0, 0, 1, 1)
-    b = (0, 1, 0, 1)
-    expected_value = [0, 1, 1, 1]
     dut.RST_N.value = 1
     await Timer(1, 'ns')
     dut.RST_N.value = 0
     await Timer(1, 'ns')
     await RisingEdge(dut.CLK)
     dut.RST_N.value = 1
-    adrv = InputDriver(dut, 'a', dut.CLK)
-    InputMonitor(dut, 'a', dut.CLK, callback=a_cover)
-    bdrv = InputDriver(dut, 'b', dut.CLK)
-    OutputDriver(dut, 'y', dut.CLK, sb_fn)
+    writedrv = InputDriver(dut, 'write', dut.CLK)
+    InputMonitor(dut, 'write', dut.CLK, callback=write_cover)
+    readdrv=OutputDriver(dut, 'read', dut.CLK, sb_fn)
+    
+    for i in range(50):
+        writelist=[]
+        writeaddr = random.randint(0,5)
+        writelist.append(writeaddr)
+        writedata = random.randint(0, 1)
+        writelist.append(writedata)    
+        writedrv.append(writelist)
+        
+        readaddr = random.randint(0,5)        
+        if case==1:
+            readaddr = 0
+        if case==2 | case==3:
+            readaddr = 3
 
-    for i in range(4):
-        adrv.append(a[i])
-        bdrv.append(b[i])
-        cover(a[i], b[i])
-    while len(expected_value) > 0:
-        await Timer(2, 'ns')
+        readdrv.append(readaddr)
+        
+        await FallingEdge(dut.CLK)
+
     coverage_db.report_coverage(cocotb.log.info, bins=True)
     coverage_file = os.path.join(
         os.getenv('RESULT_PATH', "./"), 'coverage.xml')
@@ -61,45 +71,60 @@ async def ifc_test(dut):
 
 
 class InputDriver(BusDriver):
-    _signals = ['rdy', 'en', 'data']
+    _signals = ['address', 'data', 'en', 'rdy']
 
     def __init__(self, dut, name, clk):
         BusDriver.__init__(self, dut, name, clk)
         self.bus.en.value = 0
         self.clk = clk
 
+     
     async def _driver_send(self, value, sync=True):
+        for i in range(random.randint(0, 20)):
+            await RisingEdge(self.clk)
         if self.bus.rdy.value != 1:
             await RisingEdge(self.bus.rdy)
         self.bus.en.value = 1
-        self.bus.data.value = value
+        if case==1:
+            self.bus.en.value = 0
+        self.bus.address.value = value[0]
+        if case==2:
+            self.bus.address.value = 7
+        if case==3:
+            self.bus.address.value = 0
+        self.bus.data.value = value[1]        
         await ReadOnly()
         await RisingEdge(self.clk)
         self.bus.en.value = 0
         await NextTimeStep()
-
+        
 
 class OutputDriver(BusDriver):
-    _signals = ['rdy', 'en', 'data']
-
-    def __init__(self, dut, name, clk, sb_callback):
+    _signals = ['address', 'data', 'en', 'rdy']
+    def __init__(self, dut, name, clk,sb_callback):
         BusDriver.__init__(self, dut, name, clk)
         self.bus.en.value = 0
         self.clk = clk
         self.callback = sb_callback
-        self.append(0)
+        
 
     async def _driver_send(self, value, sync=True):
-        while True:
-            if self.bus.rdy.value != 1:
-                await RisingEdge(self.bus.rdy)
-            self.bus.en.value = 1
-            await ReadOnly()
-            self.callback(self.bus.data.value)
-            await RisingEdge(self.clk)
-            await NextTimeStep()
-            self.bus.en.value = 0
 
+        for i in range(random.randint(0, 20)):
+            await RisingEdge(self.clk)
+        if self.bus.rdy.value != 1:
+            await RisingEdge(self.bus.rdy)
+        self.bus.en.value = 1
+        self.bus.address.value = value  
+        await ReadOnly()
+        self.callback(self.bus.data.value)
+        if case==1:
+            assert self.bus.data.value==1,f"incorrect case 1"
+        if case==2 | case==3:
+            assert self.bus.data.value==0,f"incorrect case 2"
+        await RisingEdge(self.clk)
+        await NextTimeStep()
+        self.bus.en.value = 0
 
 class InputMonitor(BusMonitor):
     _signals = ['rdy', 'en', 'data']
@@ -138,3 +163,4 @@ class InputMonitor(BusMonitor):
 def a_cover(state):
     cocotb.log.warning(f"state={state}")
     pass
+
